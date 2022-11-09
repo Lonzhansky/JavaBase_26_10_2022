8f84016fe95684ab8dbddfe3ba995e112e0cba19
Lonzhanskyi Denys <lonzhansky@gmail.com>
2022-11-09 20:54:21 +0200

    test second PC

--- src/main/java/com/hillel/lonzhansky/lessons/lesson3/Main.java
+++ src/main/java/com/hillel/lonzhansky/lessons/lesson3/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Lesson 3");
+        System.out.println("Lesson sdfsdfsdfsd");
+        System.out.println("fsdf 3");
+        System.out.println("Lessonsdfsdfsdf 3");
 
         int age = 10;
 
