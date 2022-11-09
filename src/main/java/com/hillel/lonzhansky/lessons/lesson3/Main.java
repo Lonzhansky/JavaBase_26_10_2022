06621f9ecd6feefdad930c7a4398d9bb621f6826
Lonzhanskyi Denys <lonzhansky@gmail.com>
2022-11-09 21:08:11 +0200

    lesson3

--- src/main/java/com/hillel/lonzhansky/lessons/lesson3/Main.java
+++ src/main/java/com/hillel/lonzhansky/lessons/lesson3/Main.java
@@ -3,12 +3,26 @@ package com.hillel.lonzhansky.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Lesson 3");
-        System.out.println("Lesson sdfsdfsdfsd");
-        System.out.println("fsdf 3");
-        System.out.println("Lessonsdfsdfsdf 3");
+//        System.out.println("Lesson 3");
+//        System.out.println("Lesson sdfsdfsdfsd");
+//        System.out.println("fsdf 3");
+//        System.out.println("Lessonsdfsdfsdf 3");
 
         int age = 10;
+
+
+        int a = 10;
+        int b = 5;
+
+        int result = a * b + 13;
+
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+
+        System.out.println("result = " + result);
 
     }
 }
